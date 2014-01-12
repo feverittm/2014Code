270d080b66581450a538554f4ffe94d51ddbba09
Floyd <floyd.moore@gmail.com>
2014-01-12 14:56:14 -0800

    Gatherer is working minus extendable portion. will finish when method of actuation is known.

--- src/edu/wpi/first/wpilibj/templates/subsystems/Catapult.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Catapult.java
@@ -13,50 +13,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @author Administrator
  */
 public class Catapult extends Subsystem {
-    private boolean isExtended;
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-    private boolean isOn;
-    private Victor myVictor;
+    
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    public void toggleOnOff() {
-        if (isOn){
-            turnOffGather();
-        }
-        if (!isOn){
-            turnOnGather();
-        }
-    }
-    public void toggleRetract() {
-        if (isExtended){
-            retractGatherer();
-        }
-        if (!isExtended){
-            extendGatherer();
-        }
-        
-    }
-    private void extendGatherer() {
-       //extend
-        isExtended = true;
-        throw new IndexOutOfBoundsException("not yet implemented");
-    }
-    private void retractGatherer() {
-        //retract
-        isExtended = false;
-        throw new IndexOutOfBoundsException("not yet implemented");
-    }
-    private void turnOnGather() {
-        myVictor.set(1);
-        isOn = true;
-    }
-    private void turnOffGather() {
-        myVictor.set(0);
-        isOn = false;
-    }
 }
--- src/edu/wpi/first/wpilibj/templates/subsystems/Gatherer.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Gatherer.java
@@ -6,6 +6,7 @@
 
 package edu.wpi.first.wpilibj.templates.subsystems;
 
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,9 +15,51 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Gatherer extends Subsystem{
   
+    private boolean isExtended;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    private boolean isOn;
+    private Victor myVictor;
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void toggleOnOff() {
+        if (isOn){
+            turnOffGather();
+        }
+        if (!isOn){
+            turnOnGather();
+        }
+    }
+    public void toggleRetract() {
+        if (isExtended){
+            retractGatherer();
+        }
+        if (!isExtended){
+            extendGatherer();
+        }
+        
+    }
+    private void extendGatherer() {
+       //extend
+        isExtended = true;
+        throw new IndexOutOfBoundsException("not yet implemented");
+    }
+    private void retractGatherer() {
+        //retract
+        isExtended = false;
+        throw new IndexOutOfBoundsException("not yet implemented");
+    }
+    private void turnOnGather() {
+        myVictor.set(1);
+        isOn = true;
+    }
+    private void turnOffGather() {
+        myVictor.set(0);
+        isOn = false;
     }
     
     
