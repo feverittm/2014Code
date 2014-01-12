cb5506d9c6f5684ff424cd7156ab3b474891d3fc
SpartanPikmin <SpartanPikmin@gmail.com>
2014-01-12 14:19:22 -0800

    The command for driving the robot.

--- src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -14,6 +14,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    
+    
+    
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
