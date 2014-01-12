8e598e82b206fcda231682f4e3f48376de1ec281
997robotics1 <997robotics1@997robotics1-HP.CSD509J.Net>
2014-01-12 14:23:02 -0800

    Added ToggleGathererOnOff

---
+++ src/edu/wpi/first/wpilibj/templates/commands/ToggleGathererOnOff.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.templates.subsystems.Gatherer;
+
+/**
+ *
+ * @author 997robotics1
+ */
+public class ToggleGathererOnOff extends CommandBase {
+    
+    public ToggleGathererOnOff() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(subGatherer);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+
+    private void requires(Gatherer subGatherer) {
+    }
+}
