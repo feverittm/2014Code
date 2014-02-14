d21b00c49aa16e03303bd3cdd8698d2f60f1b0e5
Floyd Moore <floyd.moore@gmail.com>
2014-02-14 11:23:09 -0800

    Added more documentation

--- src/edu/wpi/first/wpilibj/templates/subsystems/subcompressor.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/subcompressor.java
@@ -9,18 +9,19 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.templates.RobotMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
  *
  * @author 997robotics4
+ *
+ * Class: subcompressor Simple class to control the compressor on the robot.
+ * Starting to develop extra methods/variables to control the duty cycle of the
+ * compressor in order to prevent the compressor from overheating.
  */
 public class subcompressor extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
     private Compressor myCompressor;
     private Timer myTimer;
     private TimerTask onTask = new TimerTask() {
@@ -38,33 +39,52 @@ public class subcompressor extends Subsystem {
 
     private boolean isOn;
 
+    /**
+     *
+     * @param compressorSwitchSlot digital port for pneumatics pressure switch
+     * @param compressorSpikeSlot relay/spike port for compressor power
+     */
     public subcompressor(int compressorSwitchSlot, int compressorSpikeSlot) {
         myCompressor = new Compressor(compressorSwitchSlot, compressorSpikeSlot);
         LiveWindow.addActuator("compressor", "compressor", myCompressor);
-        // myTimer.scheduleAtFixedRate(onTask, 0, 40000);
-        //myTimer.scheduleAtFixedRate(offTask, 20000, 40000);
         myCompressor.start();
     }
 
+    /**
+     * Default command is to start the compressor.  Doing it this way instead
+     * of in the initializer will put it in a separate thread and not lock the
+     * main control thread.
+     */
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
     }
 
+    /**
+     * Start the compressor thread
+     */
     public void startCompressor() {
         myCompressor.start();
         isOn = true;
     }
 
+    /**
+     * Stop the running compressor thread
+     */
     public void stopCompressor() {
         myCompressor.stop();
         isOn = false;
     }
 
+    /**
+     *
+     * @return true if the compressor has reached the preset pressure.
+     */
     public boolean atPressure() {
         return myCompressor.getPressureSwitchValue();
     }
 
+    /**
+     * Utility function to put information on the SmartDashboard
+     */
     public void SmartDashboard() {
         SmartDashboard.putData("Compressor", this);
         SmartDashboard.putBoolean("compressor is on", isOn);
