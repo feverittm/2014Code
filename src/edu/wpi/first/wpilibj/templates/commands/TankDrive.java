cdc66b33dd749b9bb78fb13af38721e3e4531e87
Floyd Moore <floyd.moore@gmail.com>
2014-08-13 11:39:13 -0700

    fixed the formatting and javadoc for the module

--- src/edu/wpi/first/wpilibj/templates/commands/TankDrive.java
+++ src/edu/wpi/first/wpilibj/templates/commands/TankDrive.java
@@ -10,36 +10,56 @@ package edu.wpi.first.wpilibj.templates.commands;
  * @author 997robotics3
  */
 public class TankDrive extends CommandBase {
-    
+
+    /**
+     * Main command to drive robot like a tank with direct controls for each 
+     * side of the robot independently.
+     */
     public TankDrive() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(subDriveTrain);
     }
-
-    // Called just before this Command runs the first time
+    
+    /**
+     *  Called just before this Command runs the first time
+     *  There is not initialization for this command.  It could
+     *  cause the drivetrain to jerk.
+     */
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
+    /**
+     * Called repeatedly when this Command is scheduled to run
+     *  set the speed of the robot using values from the joysticks
+     */
     protected void execute() {
-      subDriveTrain.SetLeft(oi.getLeftSpeed());
-      subDriveTrain.SetRight(oi.getRightSpeed());
-        
+        subDriveTrain.SetLeft(oi.getLeftSpeed());
+        subDriveTrain.SetRight(oi.getRightSpeed());
     }
 
-    // Make this return true when this Command no longer needs to run execute()
+    /**
+     *  This command never is finished.  It is superseeded by other functions
+     * in the command stack.
+     * @return flag.  For the drivetrain code this is always false.
+     */
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
+    /**
+     * Called once after isFinished returns true
+     *  What to do when the command is finished.
+     */
     protected void end() {
- 
+
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
+    /**
+     * Called when another command which requires one or more of the same
+     * subsystems is scheduled to run
+     *
+     */
     protected void interrupted() {
         subDriveTrain.SetLeft(0);
         subDriveTrain.SetRight(0);
