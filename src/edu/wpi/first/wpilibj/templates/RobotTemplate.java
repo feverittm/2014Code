f9f58a3923b6a79e3c8210a2de7c147ea0da770e
Floyd Moore <floyd.moore@gmail.com>
2014-08-13 10:52:17 -0700

    Updated javadoc and formatting to clean up the code.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,9 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -27,15 +25,18 @@ public class RobotTemplate extends IterativeRobot {
 
     Command autonomousCommand;
 
-    // utility function to set the color of the LED to match the
-    // team's alliance color.
-    public void setLedColor() {
+    /**
+     * Utility function to set the color of the LED's on the robot
+     * to match the team's alliance color.
+     */
+        public void setLedColor() {
         if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.kRed) {
             CommandBase.myLED.redON();
         } else {
             CommandBase.myLED.blueOn();
         }
     }
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -45,17 +46,18 @@ public class RobotTemplate extends IterativeRobot {
         setLedColor();
 
         autonomousCommand = new DriveToUltrasonicThenFIre();
-        
-                
 
         // Initialize all subsystems
         CommandBase.init();
     }
 
+    /**
+     *  Initialize robot for autonomous period
+     */
     public void autonomousInit() {
         // schedule the autonomous command (example)
         autonomousCommand.start();
-    
+
         setLedColor();
     }
 
@@ -65,17 +67,24 @@ public class RobotTemplate extends IterativeRobot {
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
     }
+
+    /**
+     *  carry out functions that need to occur at the end of autonomous 
+     */
     public void disabledInit() {
         setLedColor();
     }
 
+    /**
+     * Set up to begin telop/human controlled period.
+     */
     public void teleopInit() {
-	// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
         autonomousCommand.cancel();
-        
+
         setLedColor();
         CommandBase.subGatherer.retractGatherer();
         CommandBase.subShooter.unLatch();
@@ -88,15 +97,20 @@ public class RobotTemplate extends IterativeRobot {
         Scheduler.getInstance().run();
         SmartDashboard();
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
         LiveWindow.run();
     }
+
+    /**
+     *  Set up the callback functions to update status for the
+     *  smartdashboard.
+     */
     public void SmartDashboard() {
-      //  CommandBase.myAimer.SmartDashboard();
+        //  CommandBase.myAimer.SmartDashboard();
         CommandBase.oi.SmartDashboard();
         CommandBase.subCompressor.SmartDashboard();
         CommandBase.subDriveTrain.SmartDashboard();
